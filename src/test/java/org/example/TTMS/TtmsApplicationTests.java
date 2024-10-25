929e761c912132255d8330cff307341c84fc47d6
Andrzej Radzik <andrzej.radzik@onet.pl>
2024-10-25 16:37:01 +0200

    Add Spring files

---
+++ src/test/java/org/example/TTMS/TtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.TTMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
