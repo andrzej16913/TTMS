576e7318ed77de058076e2e4f5d648e4e0ab91bb
Andrzej Radzik <andrzej.radzik@onet.pl>
2024-11-16 19:33:31 +0100

    Change Id type in API to int64

--- src/main/java/org/example/TTMS/responses/Trip.java
+++ src/main/java/org/example/TTMS/responses/Trip.java
@@ -2,8 +2,8 @@ package org.example.TTMS.responses;
 
 import java.util.Map;
 
-public record Trip(int departureStationId, String departureStationName, String departureDateTime,
-                   int arrivalStationId, String arrivalStationName, String arrivalDateTime,
+public record Trip(long departureStationId, String departureStationName, String departureDateTime,
+                   long arrivalStationId, String arrivalStationName, String arrivalDateTime,
                    Map<String, String> price, String serviceName, String vehicleType,
                    boolean reservationAvailble, boolean reservationRequired) {
 }
